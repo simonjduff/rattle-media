8daba9df5f24e34145716c6562a680e905fbb970
SimonJDuff <simon.j.duff@gmail.com>
2015-03-25 19:34:03 +0000

    setup basic logging

--- rattlemedia.py
+++ rattlemedia.py
@@ -2,10 +2,24 @@ from flask import Flask, redirect
 from flask_socketio import SocketIO, emit
 from config import Config
 from gmusicapi import Mobileclient
+import logging
+import sys
 
 application = Flask(__name__)
 application.config['SECRET_KEY'] = Config.secret_key
 socket_io = SocketIO(application)
+
+
+def setup_logging():
+    log_formatter = logging.Formatter('[%(asctime)s] %(levelname)s (%(process)d) %(module)s: %(message)s')
+    stream_handle = logging.StreamHandler(sys.stdout)
+    stream_handle.setLevel(logging.DEBUG)
+    stream_handle.setFormatter(log_formatter)
+    logger = logging.getLogger('rattlemedia')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(stream_handle)
+
+setup_logging()
 
 
 class RattleMediaController:
@@ -14,7 +28,10 @@ class RattleMediaController:
         api.login(Config.google_username, Config.google_password)
         self.__api = api
 
+        self._logger = logging.getLogger('rattlemedia')
+
     def search(self, search_term):
+        self._logger.debug('Searching for {0}'.format(search_term))
         return self.__api.search_all_access(search_term)
 
 controller = RattleMediaController()
